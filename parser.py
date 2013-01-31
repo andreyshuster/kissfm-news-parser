56de7a4246192c8d3719d6ea3d6a2171635e29e0
Andrey Shuster <novus42@gmail.com>
2013-01-31 12:39:05 +0200

    rewriten as class

--- parser.py
+++ parser.py
@@ -6,35 +6,43 @@ from urllib2 import urlopen
 from pprint import pprint
 import json
 
-url = "http://www.kissfm.ua/news.html"
+class KissFMParser:
+    def __init__(self):
+        self.structure = []
 
-doc = BeautifulSoup(urlopen(url))
+    def parse_base(self):
+        base_url = "http://www.kissfm.ua/news.html"
+        doc = BeautifulSoup(urlopen(base_url))
+        for comm in select(doc, "div.news-item-content"):
+            elem = {}
+            for item in select(comm, "a.main-item-title"):
+                elem["link"] = item["href"]
+                elem["title"] = item.string
+            for item in select(comm, "img"):
+                elem["thumb"] = item["src"]
+            for item in select(comm, "div.news-block-item-date"):
+                elem["date"] = item.string.strip()
 
-structure = []
+            self.structure.append(elem)
 
-for comm in select(doc, "div.news-item-content"):
-    elem = {}
-    for item in select(comm, "a.main-item-title"):
-        elem["link"] = item["href"]
-        elem["title"] = item.string
-        break # there are duplicates of links here
-    for item in select(comm, "img"):
-        elem["thumb"] = item["src"]
-    for item in select(comm, "div.news-block-item-date"):
-        elem["date"] = item.string.strip()
+    def parse_inner(self):
+        for item  in self.structure:
+            link = "http://www.kissfm.ua%s" % item["link"]
+            doc = BeautifulSoup(urlopen(link))
+            for comm in select(doc, "div.content"):
+                for img in select(comm, "img"):
+                    item["img"] = img["src"]
+                    break
+            for content in select(comm, "table tbody tr td div"):
+                item["content"] = "%s" % content
 
-    structure.append(elem)
+    def run(self):
+        self.parse_base()
+        self.parse_inner()
+        print self.structure
 
-for item  in structure:
-    link = "http://www.kissfm.ua%s" % item["link"]
-    doc = BeautifulSoup(urlopen(link))
+if __name__ == "__main__":
+    parser = KissFMParser()
+    parser.run()
 
-    for comm in select(doc, "div.content"):
-        for img in select(comm, "img"):
-            item["img"] = img["src"]
-            break
 
-        for content in select(comm, "table tbody tr td div"):
-             item["content"] = "%s" % content
-
-print json.dumps(structure)
